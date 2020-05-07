2d33136df1dbb7f65a74cf6a0a2f84fbaee51660
LubaRo <cnlmode@gmail.com>
2020-05-07 11:57:27 +0400

    add page for each company

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ use Slim\Factory\AppFactory;
 use Slim\Psr7\Response;
 
 $app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
 
 // Redirect and rewrite all URLs that end in a '/' to the non-trailing '/' equivalent
 $app->add(function (Request $request, RequestHandler $handler) {
@@ -42,7 +43,8 @@ $app->get('/', function (Request $request, Response $response, $args) {
     <p>You can follow by these links:</p>
     <ul>
         <li><a href="/users">Users</a></li>
-        <li><a href="companies">Companies</a></li>
+        <li><a href="/companies">Companies</a></li>
+        <li><a href="/courses/5">5 course</a></li>
     </ul>
 EOT;
     $response->getBody()->write($body);
@@ -82,16 +84,39 @@ $app->get('/companies', function (Request $request, Response $response) {
     $params = $request->getQueryParams();
 
     $page = $params['page'] ?? 1;
-    $per = $params['per'] ?? 5;
+    $per = $params['per'] ?? 10;
     $offset = ($page - 1) * $per;
 
     $requestedData = array_slice($companiesData, $offset, $per);
     $data = array_reduce($requestedData, function ($acc, $company) {
-        $acc[] = json_encode($company, JSON_PRETTY_PRINT);
+        $acc[] = "<a href=\"\\company\\{$company['id']}\">{$company['name']}</a>";
         return $acc;
     }, []);
 
     $response->getBody()->write(implode("<br>", $data));
+    return $response;
+});
+
+$app->get('/company/{id}', function (Request $request, Response $response, array $args) {
+    $companyId = $args['id'];
+    $companiesList = App\Generator::generateCompanies(100);
+    $companyData = collect($companiesList)->firstWhere('id', $companyId);
+
+    if (!$companyData) {
+        $response->getBody()->write("Page not found.. :(");
+        return $response->withStatus(404);
+    }
+    $data = '';
+    foreach ($companyData as $key => $value) {
+        $data .= "<p><b>$key:</b> $value</p>";
+    }
+    $response->getBody()->write($data);
+    return $response;
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    $response->getBody()->write("Course id: {$id}");
     return $response;
 });
 
